27952622e94f1ae430dc8a439990333612c1d38a
Nicole Abramowski <nabramow@gmail.com>
2020-02-29 16:27:43 -0500

    Add comments to FocusWrapper.unit.test.js

--- aditum/Components/__tests__/FocusWrapper.unit.test.js
+++ aditum/Components/__tests__/FocusWrapper.unit.test.js
@@ -10,9 +10,17 @@ describe('FocusWrapper component', () => {
   MockComponent.displayName = 'MockComponent';
   
   const WrappedComponent = focusWrapper(MockComponent);
+
+  /**
+   * The mount method renders the full DOM including the child components of the parent 
+   * component. Since this is a HOC we need to use mount to check that both our wrapper
+   * component renders with its child component. Simple mount calls the constructor, 
+   * render, and componentDidMount methods.
+   */
+
   const wrapper = mount(<WrappedComponent />);
 
-  it('should render the HOC', () => {
+  it('should render the HOC with mock WrappedComponent', () => {
     expect(wrapper.exists()).toBe(true);
   });
   
